a7525fea23b958b177bbd7e187f421f3eb7bce5a
walker2u <kumar99prasad@gmail.com>
2024-09-17 22:30:55 +0530

    complete sign up api.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
@@ -8,6 +9,7 @@ import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO).then(console.log("Connected to Db")).catch((err) => {
     console.log("error : ", err);
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 function SignUp() {
+    const [formData, setFormData] = useState({});
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+        console.log(formData);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const res = await fetch('http://localhost:3000/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
+            const data = await res.json();
+            if (data.success == false) {
+                setLoading(false);
+                setError(data.message);
+                return;
+            }
+            setLoading(false);
+            setError(null);
+            navigate('/signin');
+        } catch (error) {
+            setLoading(false);
+            setError(error);
+        }
+
+    }
+
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-            <form className='flex flex-col gap-4'>
-                <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
-                <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' />
-                <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' />
-                <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' onChange={handleChange} />
+                <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg' onChange={handleChange} />
+                <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' onChange={handleChange} />
+                <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign Up'}</button>
             </form>
             <div className='flex gap-2 mt-5'>
                 <p>Already have an account?</p>
@@ -19,6 +58,7 @@ function SignUp() {
                     </span>
                 </Link>
             </div>
+            {error && <p className='text-red-500 mt-5'>{error}</p>}
         </div>
     )
 }
