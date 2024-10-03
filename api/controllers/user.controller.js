940f5f3a0086bc9c37de8324367b1666337e92ca
walker2u <kumar99prasad@gmail.com>
2024-10-03 22:40:13 +0530

    craeted getUserListing api route.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcrypt from 'bcryptjs';
@@ -47,5 +48,17 @@ export const signout = async (req, res, next) => {
         res.status(201).json("User Signed Out SuccessFully!");
     } catch (error) {
         next(error);
+    }
+}
+
+export const getUserListing = async (req, res, next) => {
+    console.log('hiiiiiiiiiiiiiiiiiiii');
+
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "you can only view your listing!"))
+    try {
+        const listing = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listing);
+    } catch (error) {
+        next(next);
     }
 }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, signout, test, updateUser, getUserListing } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/', test)
 router.post('/update/:id', verifyUser, updateUser)
 router.delete('/delete/:id', verifyUser, deleteUser)
 router.post('/signout', verifyUser, signout)
+router.get('/listing/:id', verifyUser, getUserListing)
 
 export default router;
