75b935cb16c47fe1246359eaccc4260ca5d1c92b
walker2u <kumar99prasad@gmail.com>
2024-10-03 22:07:12 +0530

    completed create listing functionality.

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase.js'
 
 function CreateListing() {
+    const [files, setFiles] = useState([]);
+    const [formData, setFormData] = useState({
+        imageUrls: []
+    });
+    const [imageUploadError, setImageUploadError] = useState(null);
+    // console.log(formData);
+
+    const handleImageUpload = () => {
+        if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+            const promises = [];
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImages(files[i]));
+            }
+            Promise.all(promises).then((urls) => {
+                setFormData({ ...formData, imageUrls: formData.imageUrls.concat(urls) });
+                setImageUploadError(null);
+            }).catch((err) => {
+                setImageUploadError("Image uplaod failed (2 mb max per image)!")
+            });
+        }
+        else {
+            setImageUploadError("You can only uplaod 6 images per listing!")
+        }
+    }
+
+    const handleDeleteImage = (idx) => {
+        setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.filter((_, i) => i !== idx)
+        });
+        console.log(formData);
+    }
+
+    const storeImages = (file) => {
+        return new Promise((resolve, reject) => {
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uplaodTask = uploadBytesResumable(storageRef, file);
+            uplaodTask.on("state_changed",
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(progress);
+                },
+                (error) => {
+                    reject(error);
+                },
+                () => {
+                    getDownloadURL(uplaodTask.snapshot.ref).then((downlaodUrl) => {
+                        resolve(downlaodUrl);
+                    })
+                }
+            )
+        });
+    }
+
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>
@@ -129,18 +187,26 @@ function CreateListing() {
                         />
                         <button
                             type='button'
+                            onClick={handleImageUpload}
                             className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
                         >Upload</button>
                     </div>
-                    <p className='text-red-700 text-sm'>
-                    </p>
+                    <p className='text-red-700'>{imageUploadError && imageUploadError}</p>
+                    {
+                        formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => {
+                            return (<div key={url} className='flex justify-between p-3 border items-center'>
+                                <img src={url} alt="Listing Image" className='h-24 w-24 object-contain rounded-lg' />
+                                <button onClick={() => handleDeleteImage(index)} className='text-red-500 p-3 rounded-lg uppercase hover:opacity-95 bg-red-50' type='button'>Delete</button>
+                            </div>)
+                        })
+                    }
                     <button
                         className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
                     >Create listing
                     </button>
                 </div>
             </form>
-        </main>
+        </main >
     )
 }
 
