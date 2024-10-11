0625e7aba60c856194fac336ddee049640bf0c28
walker2u <kumar99prasad@gmail.com>
2024-10-12 00:17:06 +0530

    created listing component in search page.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 function Search() {
 
@@ -14,7 +15,7 @@ function Search() {
     });
 
     const [loading, setLoading] = useState(false);
-    const [listing, setListing] = useState({});
+    const [listing, setListing] = useState([]);
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -164,11 +165,20 @@ function Search() {
                             <option value="createdAt_asc">Oldest</option>
                         </select>
                     </div>
-                    <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>SEARCH</button>
+                    <button type='submit' className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>SEARCH</button>
                 </form>
             </div>
-            <div>
+            <div className='flex-1'>
                 <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing Results</h1>
+                <div className='p-7 flex flex-wrap gap-4'>
+                    {!loading && listing.length === 0 && (
+                        <p className='text-xl text-slate-700'>No Listing Found!</p>
+                    )}
+                    {loading && (<p className='text-xl text-slate-700 text-center w-full'>Loading...</p>)}
+                    {!loading && listing && listing.map((list) => (
+                        <ListingItem key={list._id} listing={list} />
+                    ))}
+                </div>
             </div>
         </div>
     )
