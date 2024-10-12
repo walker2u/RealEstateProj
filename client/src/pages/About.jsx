f9cf1ec2e88a6ccc9f58f0a41e68a4653eab44a8
walker2u <kumar99prasad@gmail.com>
2024-10-12 21:10:18 +0530

    completed about page.

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function About() {
     return (
-        <div>About</div>
+        <div className='py-20 px-4 max-w-6xl mx-auto'>
+            <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Indian State</h1>
+            <p className='mb-4 text-slate700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio, reiciendis quibusdam odit qui eaque sunt veritatis laboriosam animi quo nisi quos saepe quas vitae dolores soluta ad. Perspiciatis, quod tempora.</p>
+            <p className='mb-4 text-slate700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, laborum voluptate iste minima, eos perferendis eveniet praesentium aliquam earum atque inventore repellendus magnam possimus facere blanditiis ducimus et voluptatibus? Ullam.
+                Earum officiis optio recusandae atque error eveniet, facilis, dolore, fuga obcaecati architecto quo? Corrupti libero quam doloremque atque, deleniti sunt. Explicabo minima aspernatur ab magnam officia quidem nam obcaecati modi?
+                Ipsum exercitationem mollitia deserunt id, hic suscipit distinctio atque voluptatibus non consectetur, saepe provident incidunt consequatur accusamus voluptatum dolorum fugiat iste explicabo. Nihil nisi laboriosam, illo aliquid quam consequatur ipsam!
+                Quia adipisci accusantium optio, dolorum velit sit repudiandae modi illum sed consequatur cupiditate veritatis ipsa culpa fugiat pariatur iure. Incidunt dignissimos id nemo aliquid placeat dolore nam esse molestias maiores!
+                Tenetur id accusantium ducimus modi. Harum, inventore! Iste cupiditate voluptas fugiat illo minima. Perferendis dolorum nostrum iste unde esse accusantium in quam eveniet ad laudantium, reprehenderit animi inventore omnis dolorem?</p>
+            <p className='text-slate700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, laborum voluptate iste minima, eos perferendis eveniet praesentium aliquam earum atque inventore repellendus magnam possimus facere blanditiis ducimus et voluptatibus? Ullam.
+                Earum officiis optio recusandae atque error eveniet, facilis, dolore, fuga obcaecati architecto quo? Corrupti libero quam doloremque atque, deleniti sunt. Explicabo minima aspernatur ab magnam officia quidem nam obcaecati modi?
+                Ipsum exercitationem mollitia deserunt id, hic suscipit distinctio atque voluptatibus non consectetur, saepe provident incidunt consequatur accusamus voluptatum dolorum fugiat iste explicabo. Nihil nisi laboriosam, illo aliquid quam consequatur ipsam!
+                Quia adipisci accusantium optio, dolorum velit sit repudiandae modi illum sed consequatur cupiditate veritatis ipsa culpa fugiat pariatur iure. Incidunt dignissimos id nemo aliquid placeat dolore nam esse molestias maiores!
+                Tenetur id accusantium ducimus modi. Harum, inventore! Iste cupiditate voluptas fugiat illo minima. Perferendis dolorum nostrum iste unde esse accusantium in quam eveniet ad laudantium, reprehenderit animi inventore omnis dolorem?</p>
+        </div>
     )
 }
 
